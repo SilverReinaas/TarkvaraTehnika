c43b3eea5c6559cc11463355f280659acaf54742
Silver Reinaas <silver.reinaas@gmail.com>
2018-03-01 20:09:29 +0200

    -Binding exercises from db to dashboard view

--- src/main/resources/static/src/app.js
+++ src/main/resources/static/src/app.js
@@ -7,8 +7,8 @@ export class App {
 		config.map([
 		    { route: '', name: 'home', moduleId: './home', title: 'Home', nav: false },
 			{ route: 'login', name: 'login', moduleId: './login' , title: 'Log in', nav: true },
-			{ route: 'dashboard', name: 'dashboard', moduleId: './dashboard', title: 'Dashboard', nav: true, auth: true },
-			{ route: 'exercise', name: 'exercise', moduleId: './exercise', title: 'Exercise', nav: true, auth: true }
+			{ route: 'dashboard', name: 'dashboard', moduleId: './dashboard', title: 'Dashboard', nav: true },
+			{ route: 'exercise', name: 'exercise', moduleId: './exercise', title: 'Exercise', nav: true }
 		]);
 	}	
 }
